7f9f9b43d9b5548d5831ef60eba4df4f50f21097
Tom Boone <boone@wrlc.org>
2023-05-17 14:53:48 -0400

    add authorization framework

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from settings import database
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from models import (
     Institution, get_all_institutions, submit_inst_add_form, submit_inst_edit_form, get_institution_scalar,
     get_institution
 )
 from utils import db
+from functools import wraps
 import schedulers
 import os
+import jwt
 from flask_apscheduler import APScheduler
 import atexit
-
+import sys
 
 # create app
 app = Flask(__name__)
 app.config['SCHEDULER_API_ENABLED'] = True
-app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SESSION_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = database
+app.secret_key = app.config['SESSION_KEY']
 db.init_app(app)
 
 # create database
@@ -32,16 +35,61 @@ atexit.register(lambda: scheduler.shutdown())
 
 
 # Background task to update the reports
-@scheduler.task('cron', id='update_reports', minute=53)
+@scheduler.task('cron', id='update_reports', minute=1)
 def update_reports():
     with scheduler.app.app_context():
         schedulers.update_reports()
 
 
+# decorator for pages that need auth
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        else:
+            return f(*args, **kwargs)
+
+    return decorated
+
+
 # Home page
 @app.route('/')
+@auth_required
 def index():
+
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    if 'username' in session:
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/login/n', methods=['GET'])
+def new_login():
+    session.clear()
+    for cookie in request.cookies:
+        print(cookie, file=sys.stderr)
+    if 'wrt' in request.cookies:
+        encoded_token = request.cookies['wrt']
+        user_data = jwt.decode(encoded_token, app.config['SHARED_SECRET'], algorithms=['HS256'])
+        session['username'] = user_data['primary_id']
+        session['user_home'] = user_data['inst']
+        session['display_name'] = user_data['full_name']
+        return redirect(url_for('index'))
+    else:
+        return "no login cookie"
+
+
+@app.route('/logout')
+@auth_required
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 
 # List of reports page
@@ -103,4 +151,4 @@ def institution_edit(code):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context='adhoc')
---
+++ static/js/frontend.js
@@ -0,0 +1,11 @@
+// frontend js functions
+
+// login form
+function submitLogin() {
+    const sp = "https://aladin-sp.wrlc.org/simplesaml/wrlcauth/issue.php?institution="
+    let params = "&url=https://fines.wrlc.org/login/n"
+    let select = document.getElementById('user-name');
+    let institution = select.options[select.selectedIndex].value;
+    window.location.replace(sp + institution + params);
+    return false;
+}
