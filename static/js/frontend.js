788c8e06d26b937ea5d732c62414e13e067e03e9
Tom Boone <boone@wrlc.org>
2023-05-18 12:16:26 -0400

    set params on login redirect

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -3,7 +3,7 @@
 // login form
 function submitLogin() {
     const sp = "https://aladin-sp.wrlc.org/simplesaml/wrlcauth/issue.php?institution="
-    let params = "&url=https://fines.wrlc.org/login/n"
+    let params = "&url=https://exceptions.wrlc.org/login/n"
     let select = document.getElementById('user-name');
     let institution = select.options[select.selectedIndex].value;
     window.location.replace(sp + institution + params);
