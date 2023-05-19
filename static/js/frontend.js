b12f51497dbf07be03340c5c0172afebcfa71022
Tom Boone <boone@wrlc.org>
2023-05-19 15:14:07 -0400

    merge status cells

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -9,3 +9,24 @@ function submitLogin() {
     window.location.replace(sp + institution + params);
     return false;
 }
+
+function mergeCells() {
+  let db = document.getElementById("reportbody");
+  let dbRows = db.rows;
+  let lastValue = "";
+  let lastCounter = 1;
+  let lastRow = 0;
+  for (let i = 0; i < dbRows.length; i++) {
+    let thisValue = dbRows[i].cells[0].innerHTML;
+    if (thisValue == lastValue) {
+      lastCounter++;
+      dbRows[lastRow].cells[0].rowSpan = lastCounter;
+      dbRows[i].cells[0].style.display = "none";
+    } else {
+      dbRows[i].cells[0].style.display = "table-cell";
+      lastValue = thisValue;
+      lastCounter = 1;
+      lastRow = i;
+    }
+  }
+}
