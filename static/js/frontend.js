989690b83da495b3f5920be656fd3028a8a52f9b
Tom Boone <boone@wrlc.org>
2023-05-23 10:32:10 -0400

    switch merge cells function to loop

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -20,33 +20,17 @@ function mergeCells() {
 
     for (let i = 0; i < dbRows.length; i++) {
         let thisValue = dbRows[i].cells[1].innerHTML;
-        if (thisValue === lastValue) {
-            lastCounter++;
-            dbRows[lastRow].cells[0].rowSpan = lastCounter;
-            dbRows[lastRow].cells[1].rowSpan = lastCounter;
-            dbRows[lastRow].cells[2].rowSpan = lastCounter;
-            dbRows[lastRow].cells[3].rowSpan = lastCounter;
-            dbRows[lastRow].cells[4].rowSpan = lastCounter;
-            dbRows[lastRow].cells[5].rowSpan = lastCounter;
-            dbRows[lastRow].cells[6].rowSpan = lastCounter;
-            dbRows[i].cells[0].style.display = "none";
-            dbRows[i].cells[1].style.display = "none";
-            dbRows[i].cells[2].style.display = "none";
-            dbRows[i].cells[3].style.display = "none";
-            dbRows[i].cells[4].style.display = "none";
-            dbRows[i].cells[5].style.display = "none";
-            dbRows[i].cells[6].style.display = "none";
-        } else {
-            dbRows[i].cells[0].style.display = "table-cell";
-            dbRows[i].cells[1].style.display = "table-cell";
-            dbRows[i].cells[2].style.display = "table-cell";
-            dbRows[i].cells[3].style.display = "table-cell";
-            dbRows[i].cells[4].style.display = "table-cell";
-            dbRows[i].cells[5].style.display = "table-cell";
-            dbRows[i].cells[6].style.display = "table-cell";
-            lastValue = thisValue;
-            lastCounter = 1;
-            lastRow = i;
+        for (let x = 0; x < 7; x++) {
+            if (thisValue === lastValue) {
+                lastCounter++;
+                dbRows[lastRow].cells[x].rowSpan = lastCounter;
+                dbRows[i].cells[x].style.display = "none";
+            } else {
+                dbRows[i].cells[x].style.display = "table-cell";
+                lastValue = thisValue;
+                lastCounter = 1;
+                lastRow = i;
+            }
         }
     }
 }
